807b3c6094b7f839e1320da0cc7f920c5951b846
Glenn <104025672+OlePundit@users.noreply.github.com>
2022-10-04 16:03:14 -0700

    database connection for admin

--- config/admin.php
+++ config/admin.php
@@ -173,7 +173,7 @@ return [
     'database' => [
 
         // Database connection for following tables.
-        'connection' => '',
+        'connection' => 'mysql',
 
         // User tables and model.
         'users_table' => 'admin_users',
