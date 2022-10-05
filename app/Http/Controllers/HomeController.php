358b230fbb3273a340258c24beb099c53dd2d98e
Glenn <104025672+OlePundit@users.noreply.github.com>
2022-10-05 15:45:46 -0700

    homecontroller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,6 +16,8 @@ class HomeController extends Controller
         $this->middleware('auth');
     }
 
+    
+
     /**
      * Show the application dashboard.
      *
@@ -23,6 +25,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('shops.edit');
+        return view('Shops.edit');
     }
 }
