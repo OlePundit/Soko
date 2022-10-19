75eeaa5e5122b62421b1ea096d5a276496593bbd
OlePundit <glennomondi@yahoo.com>
2022-10-19 13:24:39 -0700

    editing about page

---
+++ resources/views/About/about.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-8 offset-2">
+            <h1>About Salvacar</h1>
+            <p>
+                Sellorbuyke.com, is an online marketplace where wholesalers and retailers in the wines and spirits industry can advertise their products for free, and gain access to an online market.
+                Buyers also have the opportunity to choose their favourite alcohol from a wide variety of liquor products, right from the comfort of their own homes.
+                Through availing cutting-edge technology and providing a seamless experience for our clients, we aim to increase the ease of doing business in the silicone savannah.
+            </p>
+            <button class="btn btn-primary text-white">EXPLORE</button>
+            <h2>Why Buy or Sell on SellorBuyKe?</h2>
+            <ol>
+                <li>It’s free and easy to use</li>
+                <li>We offer you the best assortment of liquor, sourced from all corners of the world. Right at the homepage, you will find some of our best deals.
+                    Use the search button to find the beverage of your choice. 
+                </li>
+                <li>No registration needed for buyers</li>
+                <li>Easy registration process for sellers</li>
+            </ol>
+
+            <h2>How to sell on Salvacar</h2>
+            <p>
+               Click on Register at the top right of the homepage, and fill in the details.
+               Once you finish and click submit, you will be redirected to your shop page, where you can upload, edit, view products and connections.
+               To edit your shop bio, click on edit shop button at the top of the page. You will be redirected to a page, where you can add your shop description; describe 
+               what you do, your location, whether or not you provide delivery services, or a link to your website (if you have one).. Once you are done, click save shop, and you will be 
+               redirected back to your shop page.
+               To create a product, click on add product button at the top of the page, you will be redirected to a page, where you will need to fill in all the product details and upload an image
+               of the item on sale. When providing the description, be as elaborate as possible. Finally, click on save product.
+               To edit your product, click on the specific product you want to change, the site will redirect to a page, where you can see an expanded version of your product, 
+               together with all the product details. Click on edit product, again the site will go to a different page, where you can edit the specific field you want to change then save.
+               Its especially important to edit the stock status field, when products go out of stock. 
+               Your shop page is like your gateway to the world. It serves as a mini-webpage containing the full catalogue of your products. Imagine you are with someone away from your shop,
+               and they ask you what you sell. You can easily share the link to your shop page, and the prospective client will be able to have a look at everything you have on offer, including their prices, and you can even organise
+               delivery services so that they don't have to physically come to your premises. 
+               Lastly, at the top of the your shop page, you have a connect button. Baically, other shop owners can connect to you, you can engage, share contacts, and even transact business.
+               You should also convince your customers to create a shop and connect to you so that they can view your products when they visit the explore page.
+               So take as mu
+            </p>
+            <p>
+            Simply browse through our used car collection to find your ideal car for sale near you. You will find all the details you need to understand the car, including the seller’s contact.
+                Call the seller to inspect your product in a public location. 
+
+Salvacar is the best used-cars website where you connect with buyers and sellers for preowned cars, salvage cars, and car accessories.
+Save time and money
+Save yourself the trouble of moving from shop to shop with our easy-to-use pre-owned vehicles website.
+You can contact your seller within the next few minutes!
+FIND YOUR CAR (This will be a CTA button)
+What makes us different from other used car markets?
+Our website is carefully designed to improve ease of use among our diverse group of clients and provide the best experience possible.
+We are the #1 online classifieds platform for second-hand cars, salvage, and spare parts.
+We aim to bridge the gap between buyers and sellers in the automotive sector.
+FIND A SELLER
+
+
+ 
+
+        </div>
+    </div>
+</div>
+@endsection
