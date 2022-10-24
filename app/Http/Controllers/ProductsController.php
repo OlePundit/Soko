b2dd080dd30e3bb3b2e98232f1ce258c0d9f43fd
OlePundit <glennomondi@yahoo.com>
2022-10-24 09:54:33 -0700

    adding computed for product update

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -53,10 +53,20 @@ class ProductsController extends Controller
 
         ]);
 
+        if (request('offer')){
+            $price = $data['price'];
+            $offer = $data['offer'];
+            $computed =  $offer-$price;
+            $newcomputed = $computed / $price;
+            $finalcomputed = round($newcomputed * 100, 0);
+            number_format((float)$finalcomputed,0);
+        }
+
     
 
         $product->update(array_merge(
             $data,
+            $finalcomputed
         ));
 
         return redirect("/p/{$product->id}");
