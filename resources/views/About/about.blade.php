d33eea5489d8d2bdea9a90ec7092881d907a1170
OlePundit <glennomondi@yahoo.com>
2022-10-24 10:19:50 -0700

    explainer paragraphs

--- resources/views/About/about.blade.php
+++ resources/views/About/about.blade.php
@@ -10,7 +10,7 @@
                 Buyers also have the opportunity to choose their favourite alcohol from a wide variety of liquor products, right from the comfort of their own homes.
                 Through availing cutting-edge technology and providing a seamless experience for our clients, we aim to increase the ease of doing business in the silicone savannah.
             </p>
-            <button class="btn btn-primary text-white">EXPLORE</button>
+            <button class="btn btn-primary text-white mb-3">EXPLORE</button>
             <h2>Why Buy or Sell on SellorBuyKe?</h2>
             <ol class="explainer">
                 <li>It’s free and easy to use</li>
@@ -22,8 +22,8 @@
             </ol>
 
             <h2>How to sell on Salvacar</h2>
-            <h3>Registering your account and creating a shop</li>
-            <ol>
+            <h3>Registering your account and creating a shop</h3>
+            <ol class="explainer">
                <li>Click on Register at the top right of the homepage, fill in the details then submit.</li>
                <li>At the top of the home page click 'My shop'</li>
                <li> You will be redirected to your shop page,where you can upload, edit, view your products and connections.</li>
@@ -32,9 +32,9 @@
                what services or products you sell, your location, whether or not you provide delivery services, or a link to your website (if you have one).Once you are done, click 'save shop', and you will be 
                redirected back to your shop page.</li>
             </ol>
-            <button class="btn btn-primary text-white">REGISTER</button>
+            <button class="btn btn-primary text-white mb-3">REGISTER</button>
             <h3>Uploading a product</h3>
-            <ol>
+            <ol class="explainer"> 
                <li>To create a product, click on 'add product' button at the top of the page.</li>
                <li>You will be redirected to the product upload page, where you will need to fill in all the product details and upload an image
                of the item on sale.</li>
@@ -42,7 +42,7 @@
                <li>Finally, click on 'save product'.</li>
             </ol>
             <h3>Editing a product</h3>
-            <ol>
+            <ol class="explainer">
                 <li>To edit your product, click on the specific product you want to change.</li>
                 <li>The site will redirect to the product page, where you will see an expanded version of your product together with its details</li> 
                 <li>Click on 'edit product'. The site will go to the product edit page, where you can edit the specific field you want to change, and then you can press 'save product' when done.</li>
@@ -50,18 +50,18 @@
             </ol>
             
 
-            <p>Your shop page is your gateway to the world. It serves as a webpage containing your full product catalogue.<p>
-            <p>Imagine you are with acquaintances, somewhere far away from your shop,
+            <p class="explainer">Your shop page is your gateway to the world. It serves as a webpage containing your full product catalogue.<p>
+            <p class="explainer">Imagine you are with acquaintances, somewhere far away from your shop,
                and one of them asks you whether you have their favourite drink at your shop. You can easily share the link to your shop page, and the prospective client will be able to have a look at everything you have on offer, inclusive of prices. You can even organise
                delivery services so that there is no need for the prospective client physically come to your premises. 
             </p>
 
             <h3>Connect</h3>
-            <p>
+            <p class="explainer">
                Lastly, at the top of your shop page, you should see a connect button. Basically, other shop owners can connect to you, you can engage, share contacts, and even transact business.
                You should also convince your customers to create a shop and connect to you so that they can view your products when they visit the explore page.
             </p>
-            <p>
+            <p class="explainer">
                 Otherwise, happy trading on SellorbyKe. Connect, transact, and enjoy!
             </p>
 
